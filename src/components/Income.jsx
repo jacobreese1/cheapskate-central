96a4dfe629acd7416e810325cde7c0e2b37e81be
Jacob Reese <jacobreese123@gmail.com>
2023-07-08 14:51:47 -0400

    add state handling for input boxes to record the variables

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -1,31 +1,73 @@
 import {
+  Box,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Income = () => {
+  const [input, setInput] = useState();
+  //create input for table to input values - source and amount
+  //input those values into an array that can be saved into a database
+  //display values from array in the table
+
+  //  const inputHandler = (event) => {
+  //     return (  );
+  //  }
+
+  const handleInput = (event) => {
+    const { value, name } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+    console.log(input)
+    
   return (
-    <TableContainer sx={{ position: "fixed", bottom: "100px", right: "100px", left: "100px" }}>
-      <Table sx={{ maxWidth: "100px"}}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Source</TableCell>
-            <TableCell>Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Authentic</TableCell>
-            <TableCell>$1000</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: "100px",
+        right: "100px",
+        left: "100px",
+      }}
+    >
+      <TableContainer>
+        <Table sx={{ maxWidth: "100px" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Source</TableCell>
+              <TableCell>Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Authentic</TableCell>
+              <TableCell>$1000</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box sx={{ maxWidth: "100%" }}>
+        <TextField
+          name="source"
+          onChange={handleInput}
+          placeholder="Source"
+        ></TextField>
+        <TextField
+          name="amount"
+          onChange={handleInput}
+          placeholder="Amount"
+        ></TextField>
+      </Box>
+    </Box>
   );
 };
 
