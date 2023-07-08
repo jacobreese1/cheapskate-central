fc119e8079068338fe639bb63239e8bf6efe6684
Jacob Reese <jacobreese123@gmail.com>
2023-07-08 13:54:24 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+
+const Header = () => {
+    return ( <Typography>Budget App</Typography> );
+}
+ 
+export default Header;
