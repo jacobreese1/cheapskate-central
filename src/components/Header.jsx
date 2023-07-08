cc02bb850b8c523f9a60a9bb583a840d0ea0453a
Jacob Reese <jacobreese123@gmail.com>
2023-07-08 14:06:01 -0400

    add styling to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,14 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 const Header = () => {
-  return <Typography variant="h3">Cheapskate Central</Typography>;
+  return (
+    <Typography
+      variant="h3"
+      sx={{ position: "fixed", top: 0, left: 0, width: "100%", p: 2 }}
+    >
+      Cheapskate Central
+    </Typography>
+  );
 };
 
 export default Header;
