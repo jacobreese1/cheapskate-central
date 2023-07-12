6fe04643132c2ff79d0d67959d65e852b081eb58
Jacob Reese <jacobreese123@gmail.com>
2023-07-12 19:17:23 -0400

    added more goals to app page for future content to add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Header from "./components/Header";
 import Income from "./components/Income";
 
 function App() {
+
+  // create expense table
+  // add expense tracking and totals 
+      // same code as income
+  // create net current amount total on the top of page
+      // income - expense
+  // add better styling to all pages making it more visually pleasing
+  // add savings goal tracking feature
+
   return (
     <div>
       <Header />
